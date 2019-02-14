7218f1f0c616606d6deae081a9731554e7628407
hawkinbl <hawkinbl@rose-hulman.edu>
2019-02-14 16:26:27 -0500

    Removed tkinter from m3 extra

--- src/m3_extra.py
+++ src/m3_extra.py
@@ -5,8 +5,6 @@ to run on the robot
 
 import rosebot as rb
 import time
-import tkinter
-from tkinter import ttk
 
 
 def m3_proximity_sensor_pick_up(rosebot):
@@ -35,43 +33,4 @@ def m3_proximity_sensor_led_shift(rosebot, rate_of_change):
         rosebot.led_system.turn_both_leds_off()
         time.sleep(rate_of_change * (rosebot.sensor_system.InfraredProximitySensor.get_distance()) / 30)
 
-def m3_get_my_frame(window, mqtt_sender):
-    """
-        Constructs and returns a frame on the given window, where the frame has
-        Button objects to run the robot's camera programs (via MQTT).
-          :type  window:       ttk.Frame | ttk.Toplevel
-          :type  mqtt_sender:  com.MqttClient
-        """
-    # Construct the frame to return:
-    frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
-    frame.grid()
 
-    # Construct the widgets on the frame:
-    frame_label = ttk.Label(frame, text="Ben's frame")
-    IR_pickup_button = ttk.Button(frame, text="Use the IR Detector to pick up an object")
-    IR_ledflash_button = ttk.Button(frame, text="Turn the LEDS on the Lego Unit on/off based on proximity to an object")
-    led_rateofchange = ttk.Entry(frame)
-    rateofchange_label = ttk.Label(frame, text="Ratio of how fast you want the LEDs to flash on/off")
-
-    # Grid the widgets:
-    frame_label.grid(row=0, column=1)
-    IR_pickup_button.grid(row=1, column=1)
-    IR_ledflash_button.grid(row=3, column=0)
-    led_rateofchange.grid(row=2, column=0)
-    rateofchange_label.grid(row=2, column=1)
-
-    # Set the Button callbacks:
-    IR_pickup_button["command"] = lambda: handle_IR_pickup(mqtt_sender)
-    IR_ledflash_button["command"] = lambda: handle_ir_ledflash(led_rateofchange.get(), mqtt_sender)
-
-    return frame
-
-
-def handle_IR_pickup(mqtt_sender):
-    print("Handle IR Pickup")
-    mqtt_sender.send_message('m3_proximity_sensor_pickup', [])
-
-
-def handle_ir_ledflash(rateofchange, mqtt_sender):
-    print("Handle IR LED Flash")
-    mqtt_sender.send_message('m3_ir_ledflash', [rateofchange])
--- src/m3_run_this_on_laptop.py
+++ src/m3_run_this_on_laptop.py
@@ -46,6 +46,7 @@ def main():
     color_frame = shared_gui.get_color_frame(main_frame, mqtt_sender)
     camera_frame = shared_gui.get_camera_frame(main_frame, mqtt_sender)
     IR_frame = shared_gui.get_proximity_frame(main_frame, mqtt_sender)
+    my_frame = m3_get_my_frame(main_frame, mqtt_sender)
     # -------------------------------------------------------------------------
     # Frames that are particular to my individual contributions to the project.
     # -------------------------------------------------------------------------
@@ -57,7 +58,7 @@ def main():
     # -------------------------------------------------------------------------
     color_frame.grid(row=0, column=1)
     camera_frame.grid(row=1, column=1)
-    IR_frame.grid(row=2, column=1)
+    my_frame.grid(row=2, column=1)
     # -------------------------------------------------------------------------
     # The event loop:
     # -------------------------------------------------------------------------
@@ -79,7 +80,46 @@ def grid_frames(teleop_frame, arm_frame, control_frame, sound_frame):
     control_frame.grid(row=2, column=0)
     sound_frame.grid(row=3, column=0)
 
+def m3_get_my_frame(window, mqtt_sender):
+    """
+        Constructs and returns a frame on the given window, where the frame has
+        Button objects to run the robot's camera programs (via MQTT).
+          :type  window:       ttk.Frame | ttk.Toplevel
+          :type  mqtt_sender:  com.MqttClient
+        """
+    # Construct the frame to return:
+    frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
+    frame.grid()
 
+    # Construct the widgets on the frame:
+    frame_label = ttk.Label(frame, text="Ben's frame")
+    IR_pickup_button = ttk.Button(frame, text="Use the IR Detector to pick up an object")
+    IR_ledflash_button = ttk.Button(frame, text="Turn the LEDS on the Lego Unit on/off based on proximity to an object")
+    led_rateofchange = ttk.Entry(frame)
+    rateofchange_label = ttk.Label(frame, text="Ratio of how fast you want the LEDs to flash on/off")
+
+    # Grid the widgets:
+    frame_label.grid(row=0, column=1)
+    IR_pickup_button.grid(row=1, column=1)
+    IR_ledflash_button.grid(row=3, column=0)
+    led_rateofchange.grid(row=2, column=0)
+    rateofchange_label.grid(row=2, column=1)
+
+    # Set the Button callbacks:
+    IR_pickup_button["command"] = lambda: handle_IR_pickup(mqtt_sender)
+    IR_ledflash_button["command"] = lambda: handle_ir_ledflash(led_rateofchange.get(), mqtt_sender)
+
+    return frame
+
+
+def handle_IR_pickup(mqtt_sender):
+    print("Handle IR Pickup")
+    mqtt_sender.send_message('m3_proximity_sensor_pickup', [])
+
+
+def handle_ir_ledflash(rateofchange, mqtt_sender):
+    print("Handle IR LED Flash")
+    mqtt_sender.send_message('m3_ir_ledflash', [rateofchange])
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
