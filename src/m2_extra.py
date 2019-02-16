b841023e2b41eb5d97d7a252d063ff1d632b31f9
Srkline3 <klinesr@rose-hulman.edu>
2019-02-16 15:49:48 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/m2_extra.py
+++ src/m2_extra.py
@@ -12,16 +12,16 @@ def find_object_ir(robot, starting_frequency, rate_of_increase):
     frequency = starting_frequency
     while True:
         ave = 0
-        for k in range(8):
+        for k in range(4):
             ave = ave + robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
-        ave = ave / 8
+        ave = ave / 4
 
         frequency = frequency + (rate_of_increase/ave)
-        robot.sound_system.tone_maker(frequency, 500)
+        robot.sound_system.tone_maker.play_tone(frequency, 500)
 
         if ave <=3:
             robot.drive_system.stop()
-            robot.ArmAndClaw.raise_arm()
+            robot.arm_and_claw.raise_arm()
             break
 
 
@@ -35,12 +35,12 @@ def find_object_camera(robot, starting_frequency, rate_of_increase, clockwise):
     frequency = starting_frequency
     while True:
         ave = 0
-        for k in range(8):
+        for k in range(4):
             ave = ave + robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
-        ave = ave / 8
+        ave = ave / 4
 
         frequency = frequency + (rate_of_increase/ave)
-        robot.sound_system.tone_maker(frequency, 500)
+        robot.sound_system.tone_maker.play_tone(frequency, 500)
 
         if ave <=3:
             robot.drive_system.stop()
